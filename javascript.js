8c6fe04f961868d802e758469c6a5585a2e861dd
Aryan Nath <74306760+natharyan@users.noreply.github.com>
2021-05-09 19:01:46 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -8,19 +8,32 @@ function myFunction() {
         document.getElementById('faceimg').src='images/2(1).png'
     } else if(x=='tim'){
         document.getElementById('faceimg').src='images/3(1).png'
-    }   else if(x=='sarah'){
-        document.getElementById('faceimg').src='images/4(1).png'
-    }   else if(x=='karen'){
-        document.getElementById('faceimg').src='images/5(1).png'
-    }   else {
-        document.getElementById('faceimg').src='images/6(1).png'
-    }
-    if(y=='superman1'){
-        document.getElementById('bodyimg').src='images/superman1.png'
-    } else if(y=='superman2'){
-        document.getElementById('bodyimg').src='images/superman2.png'
+    }  
+    if(y=='outfit 1'){
+        document.getElementById('bodyimg').src='images/imageonline-co-split-image_1_-removebg-preview.png'
+    } else if(y=='outfit 2'){
+        document.getElementById('bodyimg').src='images/imageonline-co-split-image_2_-removebg-preview.png'
     } else {
-        document.getElementById('bodyimg').src='images/superwomen.png'
+        document.getElementById('bodyimg').src='images/imageonline-co-split-image-removebg-preview.png'
+    }   
+}       
+
+function myFunction1() {
+    var x = document.getElementById('face1').value;
+    var y = document.getElementById('body1').value;
+    if(x=='sarah'){
+        document.getElementById('faceimg1').src='images/4(1).png'
+    }   else if(x=='karen'){
+        document.getElementById('faceimg1').src='images/5(1).png'
+    }   else {
+        document.getElementById('faceimg1').src='images/6(1).png'
+    }
+    if(y=='outfit 1'){
+        document.getElementById('bodyimg1').src='images/imageonline-co-split-image_1_-removebg-preview.png'
+    } else if(y=='outfit 2'){
+        document.getElementById('bodyimg1').src='images/imageonline-co-split-image_2_-removebg-preview.png'
+    } else {
+        document.getElementById('bodyimg1').src='images/imageonline-co-split-image-removebg-preview.png'
     }   
 }       
 
